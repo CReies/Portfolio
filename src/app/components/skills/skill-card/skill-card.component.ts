fe407b2282bb4fd131815e2129388cf2500433f0
= <ccreyesmolina@gmail.com>
2024-04-14 00:32:48 -0500

    Add data to the skills components

--- src/app/components/skills/skill-card/skill-card.component.ts
+++ src/app/components/skills/skill-card/skill-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITechnology } from '../../../../Interfaces/ITechnology';
+import { SvgComponent } from '../../svg/svg.component';
 
 @Component({
-  selector: 'app-skill-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './skill-card.component.html',
-  styleUrl: './skill-card.component.css'
+	selector: 'app-skill-card',
+	standalone: true,
+	imports: [SvgComponent],
+	templateUrl: './skill-card.component.html',
+	styleUrl: './skill-card.component.css',
 })
 export class SkillCardComponent {
-
+	@Input() skill!: ITechnology;
 }
