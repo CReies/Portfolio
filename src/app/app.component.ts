9ca66c2a86a3a3de07a30e716a25457d1326acbd
= <ccreyesmolina@gmail.com>
2024-04-13 23:43:36 -0500

    Import HttpClientModule on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, HeaderComponent, HomeComponent, ProjectsComponent],
+	imports: [
+		RouterOutlet,
+		HeaderComponent,
+		HomeComponent,
+		ProjectsComponent,
+		HttpClientModule,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
