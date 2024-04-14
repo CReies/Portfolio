6580997bce36a34e058971a36db8a5d1709e6fc3
= <ccreyesmolina@gmail.com>
2024-04-14 18:35:11 -0500

    Implement conditional rendering and add logic to the responsive menu

--- src/app/components/header/responsive-menu/responsive-menu.component.ts
+++ src/app/components/header/responsive-menu/responsive-menu.component.ts
@@ -2,12 +2,22 @@ import { Component } from '@angular/core';
 import { SvgComponent } from '../../svg/svg.component';
 
 @Component({
-  selector: 'app-responsive-menu',
-  standalone: true,
-  imports: [SvgComponent],
-  templateUrl: './responsive-menu.component.html',
-  styleUrl: './responsive-menu.component.css'
+	selector: 'app-responsive-menu',
+	standalone: true,
+	imports: [SvgComponent],
+	templateUrl: './responsive-menu.component.html',
+	styleUrl: './responsive-menu.component.css',
 })
 export class ResponsiveMenuComponent {
+	closeMenuDisabled = true;
 
+	openMenu = (): void => {
+		const menu = document.querySelector('#headerNavbar');
+		const themeSwitch = document.querySelector('#themeSwitchContainer');
+
+		menu?.classList.toggle('header__navbar--disabled');
+		themeSwitch?.classList.toggle('theme-switch--disabled');
+
+		this.closeMenuDisabled = !this.closeMenuDisabled;
+	};
 }
