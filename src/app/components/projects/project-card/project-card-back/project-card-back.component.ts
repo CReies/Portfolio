60c55f702b5710da1f42c5beb4289fb40aa016a6
= <ccreyesmolina@gmail.com>
2024-04-13 23:45:48 -0500

    Use the svg component to render dynamically the technologies used on the projects

--- src/app/components/projects/project-card/project-card-back/project-card-back.component.ts
+++ src/app/components/projects/project-card/project-card-back/project-card-back.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IProjectCardBackData } from '../../../../../Interfaces/IProjectCardBackData';
+import { SvgComponent } from '../../../svg/svg.component';
 
 @Component({
 	selector: 'app-project-card-back',
 	standalone: true,
-	imports: [],
+	imports: [SvgComponent],
 	templateUrl: './project-card-back.component.html',
 	styleUrl: './project-card-back.component.css',
 })
