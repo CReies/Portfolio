42567cd4bf614bd08a8d2b134d600a9730d89fc2
= <ccreyesmolina@gmail.com>
2024-04-14 01:01:05 -0500

    Add social links to footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ISocialLink } from '../../../Interfaces/ISocialLink';
 
 @Component({
 	selector: 'app-footer',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 	templateUrl: './footer.component.html',
 	styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+	socialLinks: ISocialLink[] = [
+		{ name: 'GitHub', link: 'https://github.com/CReies' },
+		{
+			name: 'LinkedIn',
+			link: 'https://www.linkedin.com/in/cristian-reyes-a66b01167/',
+		},
+		{ name: 'Instagram', link: 'https://www.instagram.com/c._.reyes_/' },
+	];
+}
