0298b9c7a8e806cf40677b339843e30071f6e712
= <ccreyesmolina@gmail.com>
2024-04-14 18:06:08 -0500

    Update theme switch to use app svg component

--- src/app/components/header/theme-switch/theme-switch.component.ts
+++ src/app/components/header/theme-switch/theme-switch.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SvgComponent } from '../../svg/svg.component';
 
 @Component({
-  selector: 'app-theme-switch',
-  standalone: true,
-  imports: [],
-  templateUrl: './theme-switch.component.html',
-  styleUrl: './theme-switch.component.css'
+	selector: 'app-theme-switch',
+	standalone: true,
+	imports: [SvgComponent],
+	templateUrl: './theme-switch.component.html',
+	styleUrl: './theme-switch.component.css',
 })
-export class ThemeSwitchComponent {
-
-}
+export class ThemeSwitchComponent {}
