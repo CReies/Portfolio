7f5b116514480d93c3648c92d8b514c40b737948
CReies <ccreyesmolina@gmail.com>
2024-10-24 15:41:38 -0500

    Refactor navigation links in header component to dynamically generate navigation items from an array

--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  navItems = [
+    { id: 0, name: "Home", active: false, link: "#homeSection" },
+    { id: 1, name: "Projects", active: false, link: "#projectsSection" },
+    { id: 2, name: "Skills", active: false, link: "#skillsSection" },
+    { id: 3, name: "Contact Me", active: true, link: "https://www.linkedin.com/in/cristian-reyes-a66b01167/", blank: true }
+  ];
 }
